65ceaef2c8c25f21b6a97026de4c82972dc653ca
xiaofeng1990 <2840824715@qq.com>
2025-02-11 18:14:15 +0800

    add yolov5 postprocess batch gpu

--- 2.cuda-runtime-api/2.12-yolov5-postprocess/src/decode_gpu.cu
+++ 2.cuda-runtime-api/2.12-yolov5-postprocess/src/decode_gpu.cu
@@ -57,6 +57,65 @@ static __global__ void decode_kernel(float *predict, int num_bboxes, int num_cla
     *pout_item++ = 1; // 1 = keep, 0 = ignore
 }
 
+static __global__ void decode_kernel_batch(float *predict, size_t predict_size, int num_bboxes, int num_classes,
+                                           float confidence_threshold, float *invert_affine_matrix, float *parray, size_t output_size,
+                                           size_t batch, int max_objects, int NUM_BOX_ELEMENT)
+
+{
+    int dx = blockDim.x * blockIdx.x + threadIdx.x;
+    int dy = blockDim.y * blockIdx.y + threadIdx.y;
+    if (dx >= num_bboxes || dy >= batch)
+        return;
+
+    // 获得当前线程的box信息
+    float *predict_index = predict + predict_size * dy;
+    float *parray_index = parray + output_size * dy;
+
+    float *pitem = predict_index + (5 + num_classes) * dx;
+    float objectness = pitem[4];
+    if (objectness < confidence_threshold)
+        return;
+
+    // cx, cy, width, height, objness, classification*80
+    float *class_confidence = pitem + 5;
+    float confidence = *class_confidence++;
+    int label = 0;
+    for (int i = 1; i < num_classes; ++i, ++class_confidence)
+    {
+        if (*class_confidence > confidence)
+        {
+            confidence = *class_confidence;
+            label = i;
+        }
+    }
+    // 当前网格中有目标，且为某一个类别的的置信度
+    confidence *= objectness;
+    if (confidence < confidence_threshold)
+        return;
+    // parray第一个元素记录保存了多少个box
+    int index = atomicAdd(parray_index, 1);
+    if (index >= max_objects)
+        return;
+    float cx = *pitem++;
+    float cy = *pitem++;
+    float width = *pitem++;
+    float height = *pitem++;
+    float left = cx - width * 0.5f;
+    float top = cy - height * 0.5f;
+    float right = cx + width * 0.5f;
+    float bottom = cy + height * 0.5f;
+
+    // left, top, right, bottom, confidence, class, keepflag
+    float *pout_item = parray_index + 1 + index * NUM_BOX_ELEMENT;
+    *pout_item++ = left;
+    *pout_item++ = top;
+    *pout_item++ = right;
+    *pout_item++ = bottom;
+    *pout_item++ = confidence;
+    *pout_item++ = label;
+    *pout_item++ = 1; // 1 = keep, 0 = ignore
+}
+
 static __device__ float box_iou(
     float aleft, float atop, float aright, float abottom,
     float bleft, float btop, float bright, float bbottom)
@@ -110,6 +169,45 @@ static __global__ void fast_nms_kernel(float *bboxes, int max_objects, float thr
         }
     }
 }
+
+static __global__ void fast_nms_kernel_batch(float *bboxes, int max_objects, float threshold, size_t output_size, int batch, int NUM_BOX_ELEMENT)
+{
+
+    int dx = (blockDim.x * blockIdx.x + threadIdx.x);
+    int dy = (blockDim.x * blockIdx.x + threadIdx.x);
+    int count = min((int)*bboxes, max_objects);
+    if (dx >= count || dy >= batch)
+        return;
+
+    float *bboxes_index = bboxes + output_size * dy;
+    // left, top, right, bottom, confidence, class, keepflag
+    float *pcurrent = bboxes_index + 1 + dx * NUM_BOX_ELEMENT;
+
+    for (int i = 0; i < count; ++i)
+    {
+        // 不和自己比较，如果是不同的类别也不比较
+        float *pitem = bboxes_index + 1 + i * NUM_BOX_ELEMENT;
+        if (i == dx || pcurrent[5] != pitem[5])
+            continue;
+
+        if (pitem[4] >= pcurrent[4])
+        {
+            if (pitem[4] == pcurrent[4] && i < dx)
+                continue;
+
+            float iou = box_iou(
+                pcurrent[0], pcurrent[1], pcurrent[2], pcurrent[3],
+                pitem[0], pitem[1], pitem[2], pitem[3]);
+
+            if (iou > threshold)
+            {
+                pcurrent[6] = 0; // 1=keep, 0=ignore
+                return;
+            }
+        }
+    }
+}
+
 void decode_kernel_invoker(float *predict, int num_bboxes, int num_classes, float confidence_threshold,
                            float nms_threshold, float *invert_affine_matrix, float *parray, int max_objects,
                            int NUM_BOX_ELEMENT, cudaStream_t stream)
@@ -123,4 +221,28 @@ void decode_kernel_invoker(float *predict, int num_bboxes, int num_classes, floa
     block = max_objects > 512 ? 512 : max_objects;
     grid = (max_objects + block - 1) / block;
     fast_nms_kernel<<<grid, block, 0, stream>>>(parray, max_objects, nms_threshold, NUM_BOX_ELEMENT);
+}
+
+void decode_kernel_invoker_batch(float *predict, size_t predict_size, int num_bboxes, int num_classes, float confidence_threshold,
+                                 float nms_threshold, float *invert_affine_matrix, float *parray, size_t output_size, int max_objects,
+                                 int NUM_BOX_ELEMENT, size_t batch, cudaStream_t stream)
+{
+    auto block_size = num_bboxes > 1024 ? 1024 : num_bboxes;
+
+    dim3 grid((num_bboxes + block_size - 1) / block_size, batch);
+    dim3 block(block_size);
+
+    // printf("grid size = %d * %d * %d \n", grid.x, grid.y, grid.z);
+    // printf("block size = %d * %d * %d \n", block.x, block.y, block.z);
+    // decode_kernel_batch<<<grid, block, 0, stream>>>(predict, predict_size, num_bboxes, num_classes, confidence_threshold,
+    //                                                 invert_affine_matrix, parray, output_size, batch, max_objects, NUM_BOX_ELEMENT);
+    decode_kernel_batch<<<grid, block, 0, nullptr>>>(predict, predict_size, num_bboxes, num_classes, confidence_threshold,
+                                                     invert_affine_matrix, parray, output_size, batch, max_objects, NUM_BOX_ELEMENT);
+
+    block_size = max_objects > 1024 ? 1024 : max_objects;
+    dim3 grid_nms((max_objects + block_size - 1) / block_size, batch);
+    dim3 block_nms(block_size);
+    // printf("grid_nms size = %d * %d * %d \n", grid_nms.x, grid_nms.y, grid_nms.z);
+    // printf("block_nms size = %d * %d * %d \n", block_nms.x, block_nms.y, block_nms.z);
+    fast_nms_kernel_batch<<<grid_nms, block_nms, 0, nullptr>>>(parray, max_objects, nms_threshold, output_size, batch, NUM_BOX_ELEMENT);
 }
--- 2.cuda-runtime-api/2.12-yolov5-postprocess/src/main.cpp
+++ 2.cuda-runtime-api/2.12-yolov5-postprocess/src/main.cpp
@@ -9,6 +9,14 @@
 
 #define checkRuntime(op) __check_cuda_runtime((op), #op, __FILE__, __LINE__)
 
+void decode_kernel_invoker(float *predict, int num_bboxes, int num_classes, float confidence_threshold,
+                           float nms_threshold, float *invert_affine_matrix, float *parray, int max_objects,
+                           int NUM_BOX_ELEMENT, cudaStream_t stream);
+
+void decode_kernel_invoker_batch(float *predict, size_t predict_size, int num_bboxes, int num_classes, float confidence_threshold,
+                                 float nms_threshold, float *invert_affine_matrix, float *parray, size_t output_size, int max_objects,
+                                 int NUM_BOX_ELEMENT, size_t batch, cudaStream_t stream);
+
 bool __check_cuda_runtime(cudaError_t code, const char *op, const char *file, int line)
 {
 
@@ -99,16 +107,11 @@ std::vector<Box> decode_cpu(float *predict, int rows, int clos, float confidence
 
     return box_result;
 }
-void decode_kernel_invoker(float *predict, int num_bboxes, int num_classes, float confidence_threshold,
-                           float nms_threshold, float *invert_affine_matrix, float *parray, int max_objects,
-                           int NUM_BOX_ELEMENT, cudaStream_t stream);
 
 std::vector<Box> decode_gpu(float *predict, int rows, int cols,
                             float confidence_threshold = 0.25f,
                             float nms_threshold = 0.45f)
 {
-    auto systemtime = std::chrono::system_clock::now();
-    uint64_t timestamp1(std::chrono::duration_cast<std::chrono::microseconds>(systemtime.time_since_epoch()).count());
 
     std::vector<Box> box_result;
     cudaStream_t stream = nullptr;
@@ -122,6 +125,10 @@ std::vector<Box> decode_gpu(float *predict, int rows, int cols,
     checkRuntime(cudaMalloc(&predict_device, rows * cols * sizeof(float)));
     checkRuntime(cudaMalloc(&output_device, sizeof(float) + max_objects * NUM_BOX_ELEMENT * sizeof(float)));
     checkRuntime(cudaMallocHost(&output_host, sizeof(float) + max_objects * NUM_BOX_ELEMENT * sizeof(float)));
+
+    auto systemtime = std::chrono::system_clock::now();
+    uint64_t timestamp1(std::chrono::duration_cast<std::chrono::microseconds>(systemtime.time_since_epoch()).count());
+
     // 异步将host数据cpy到device
     checkRuntime(cudaMemcpyAsync(predict_device, predict, rows * cols * sizeof(float), cudaMemcpyHostToDevice, stream));
     decode_kernel_invoker(
@@ -145,22 +152,104 @@ std::vector<Box> decode_gpu(float *predict, int rows, int cols,
                 ptr[0], ptr[1], ptr[2], ptr[3], ptr[4], (int)ptr[5]);
         }
     }
+    systemtime = std::chrono::system_clock::now();
+    uint64_t timestamp2(std::chrono::duration_cast<std::chrono::microseconds>(systemtime.time_since_epoch()).count());
+    printf("gpu yolov5 postprocess %ld ns\n", timestamp2 - timestamp1);
+
     checkRuntime(cudaStreamDestroy(stream));
     checkRuntime(cudaFree(predict_device));
     checkRuntime(cudaFree(output_device));
     checkRuntime(cudaFreeHost(output_host));
 
+    return box_result;
+}
+
+std::vector<std::vector<Box>> decode_gpu_batch(float *predict, int rows, int cols, int batch,
+                                               float confidence_threshold = 0.25f, float nms_threshold = 0.45f)
+{
+    std::vector<std::vector<Box>> box_result_batch;
+    cudaStream_t stream = nullptr;
+    checkRuntime(cudaStreamCreate(&stream));
+    float *predict_device = nullptr;
+    float *predict_host = nullptr;
+
+    float *output_device = nullptr;
+    float *output_host = nullptr;
+    int max_objects = 1000;
+    // left, top, right, bottom, confidence, class, keepflag
+    int NUM_BOX_ELEMENT = 7;
+    size_t predict_fream_size = rows * cols;
+
+    size_t predict_size = batch * predict_fream_size * sizeof(float);
+
+    size_t output_fream_size = 1 + max_objects * NUM_BOX_ELEMENT;
+    size_t output_size = batch * output_fream_size * sizeof(float);
+
+    checkRuntime(cudaMalloc(&predict_device, predict_size));
+    checkRuntime(cudaMallocHost(&predict_host, predict_size));
+
+    checkRuntime(cudaMalloc(&output_device, output_size));
+    checkRuntime(cudaMallocHost(&output_host, output_size));
+
+    float *psrc_host_index;
+
+    for (size_t i = 0; i < batch; i++)
+    {
+
+        psrc_host_index = predict_host + predict_fream_size * i;
+
+        memcpy(psrc_host_index, predict, predict_fream_size);
+    }
+    // printf("gpu batch postprocess memcpy\n");
+
+    // 异步将host数据cpy到device
+    // checkRuntime(cudaMemcpyAsync(predict_device, predict_host, predict_size, cudaMemcpyHostToDevice, stream));
+    checkRuntime(cudaMemcpy(predict_device, predict_host, predict_size, cudaMemcpyHostToDevice));
+    auto systemtime = std::chrono::system_clock::now();
+    uint64_t timestamp1(std::chrono::duration_cast<std::chrono::microseconds>(systemtime.time_since_epoch()).count());
+    decode_kernel_invoker_batch(
+        predict_device, predict_fream_size, rows, cols - 5, confidence_threshold,
+        nms_threshold, nullptr, output_device, output_fream_size, max_objects, NUM_BOX_ELEMENT, batch, stream);
+
+    checkRuntime(cudaDeviceSynchronize());
+    // checkRuntime(cudaMemcpyAsync(output_host, output_device, output_size, cudaMemcpyDeviceToHost, stream));
+
+    checkRuntime(cudaMemcpy(output_host, output_device, output_size, cudaMemcpyDeviceToHost));
+
+    // checkRuntime(cudaStreamSynchronize(stream));
     systemtime = std::chrono::system_clock::now();
     uint64_t timestamp2(std::chrono::duration_cast<std::chrono::microseconds>(systemtime.time_since_epoch()).count());
-
     printf("gpu yolov5 postprocess %ld ns\n", timestamp2 - timestamp1);
+    float *output_host_index;
+    for (size_t i = 0; i < batch; i++)
+    {
+        output_host_index = output_host + output_fream_size * i;
+        int num_boxes = std::min((int)output_host_index[0], max_objects);
+        std::vector<Box> box_result;
+        for (int i = 0; i < num_boxes; ++i)
+        {
+            float *ptr = output_host_index + 1 + NUM_BOX_ELEMENT * i;
+            int keep_flag = ptr[6];
+            if (keep_flag)
+            {
+                box_result.emplace_back(
+                    ptr[0], ptr[1], ptr[2], ptr[3], ptr[4], (int)ptr[5]);
+            }
+        }
+        box_result_batch.emplace_back(box_result);
+    }
 
-    return box_result;
+    checkRuntime(cudaStreamDestroy(stream));
+    checkRuntime(cudaFree(predict_device));
+    checkRuntime(cudaFree(output_device));
+    checkRuntime(cudaFreeHost(output_host));
+    checkRuntime(cudaFreeHost(predict_host));
+    return box_result_batch;
 }
 
 int main()
 {
-    std::string root_path("/home/wxf/workspace/tensorrt-tutorial/2.cuda-runtime-api/2.12-yolov5-postprocess/data/");
+    std::string root_path("/home/xintent/workspace/wxf/tensorrt-tutorial/2.cuda-runtime-api/2.12-yolov5-postprocess/data/");
     std::string data_file = root_path + "predict.data";
     std::string image_file = root_path + "input-image.jpg";
     auto data = load_file(data_file);
@@ -169,8 +258,14 @@ int main()
     int nelem = data.size() / sizeof(float);
     int ncols = 85;
     int nrows = nelem / ncols;
-    // auto boxes = decode_cpu(ptr, nrows, ncols);
-    auto boxes = decode_gpu(ptr, nrows, ncols);
+    auto boxes = decode_cpu(ptr, nrows, ncols);
+    // auto boxes = decode_gpu(ptr, nrows, ncols);
+    int batch = 5;
+    printf("data.size() = %ld\n", data.size());
+    printf("nelem = %ld\n", nelem);
+    printf("ncols = %ld nrows = %d\n", ncols, nrows);
+    auto boxes_batch = decode_gpu_batch(ptr, nrows, ncols, batch);
+
     for (auto &box : boxes)
     {
         cv::rectangle(image, cv::Point(box.left, box.top), cv::Point(box.right, box.bottom), cv::Scalar(0, 255, 0), 2);
@@ -178,6 +273,17 @@ int main()
     }
     std::string save_image_file = root_path + "image-draw.jpg";
     cv::imwrite(save_image_file, image);
+
+    for (size_t i = 0; i < batch; i++)
+    {
+        for (auto &box : boxes_batch[i])
+        {
+            cv::rectangle(image, cv::Point(box.left, box.top), cv::Point(box.right, box.bottom), cv::Scalar(0, 255, 0), 2);
+            cv::putText(image, cv::format("%.2f", box.confidence), cv::Point(box.left, box.top - 7), 0, 0.8, cv::Scalar(0, 0, 255), 2, 16);
+        }
+        std::string save_image_file = "output_draw_" + std::to_string(i) + ".jpg";
+        cv::imwrite(save_image_file, image);
+    }
 
     return 0;
 }
